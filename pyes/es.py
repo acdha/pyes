fc722cb114a088a4486621fe4bf81a8b1b8ee174
Chris Adams <chris@improbable.org>
2011-03-28 22:53:28 -0400

    Performance boost: use simplejson over stdlib json
    This boosts performance by a factor of 3-4x

--- pyes/es.py
+++ pyes/es.py
@@ -5,11 +5,11 @@ __author__ = 'Alberto Paro, Robert Eanes, Matt Dennewitz'
 __all__ = ['ES', 'file_to_attachment', 'decode_json']
 
 try:
-    # For Python >= 2.6
-    import json
-except ImportError:
     # For Python < 2.6 or people using a newer version of simplejson
     import simplejson as json
+except ImportError:
+    # For Python >= 2.6
+    import json
 
 import logging
 from datetime import date, datetime
